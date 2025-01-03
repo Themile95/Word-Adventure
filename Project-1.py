c89f4e868b1bf60c6a8663a1b8b034190c2e74e3
Themile95 <jhonwesen8@gmail.com>
2025-01-03 11:42:29 -0800

    Add files via upload

---
+++ Project-1.py
@@ -0,0 +1,17 @@
+#calculator
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+op = input("Enter the operator: ")
+
+#if_statement
+
+if op == "+":
+    print(str("The answer is") (num_1 + num_2))
+if op == "-":
+    print(str("The answer is") (num_1 - num_2))
+if op == "/":
+    print(str("The answer is") (num_1 / num_2))
+if op == "*":
+    print(str("The answer is") (num_1 * num_2))
+
